e017bf707678904fb02a0af6cb414afb276fbbcc
XavTo <xaviertonnellier6@outlook.fr>
2025-10-15 17:34:44 +0200

    [FIX] fix kernel

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -21,6 +21,7 @@ class Kernel extends HttpKernel
         \Illuminate\Foundation\Http\Middleware\ValidatePostSize::class,
         \App\Http\Middleware\TrimStrings::class,
         \Illuminate\Foundation\Http\Middleware\ConvertEmptyStringsToNull::class,
+        'cron.secret' => \App\Http\Middleware\EnsureCronSecret::class,
     ];
 
     /**
@@ -43,7 +44,6 @@ class Kernel extends HttpKernel
             'throttle:60,1',
             \Illuminate\Routing\Middleware\SubstituteBindings::class,
         ],
-        'cron.secret' => \App\Http\Middleware\EnsureCronSecret::class,
     ];
 
     /**
