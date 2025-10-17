ca98828be5cc79ac3d8138aae79efda7728a6559
Xavier To <xaviertonneller6@outlook.fr>
2025-10-17 22:24:06 +0200

    [FEAT] add scheduler

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -4,6 +4,7 @@ namespace App\Console;
 
 use Illuminate\Console\Scheduling\Schedule;
 use Illuminate\Foundation\Console\Kernel as ConsoleKernel;
+use Sendportal\Base\Facades\Sendportal;
 
 class Kernel extends ConsoleKernel
 {
@@ -24,6 +25,7 @@ class Kernel extends ConsoleKernel
     protected function schedule(Schedule $schedule)
     {
         // $schedule->command('inspire')->hourly();
+        Sendportal::schedule($schedule);
     }
 
     /**
